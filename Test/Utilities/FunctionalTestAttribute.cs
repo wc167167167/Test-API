ea31bccf86335e9a696ceb7952d79f7198f96bb2
wc167167167 <jixuan.xiong@gmail.com>
2017-09-20 12:14:29 +0100

    Firt Commit

---
+++ Test/Utilities/FunctionalTestAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Test.Utilities
+{
+    /// <summary>
+    ///     An attribute of functional testing.
+    /// </summary>
+    internal class FunctionalTestAttribute : TestBaseAttribute
+    {
+        public FunctionalTestAttribute() : base("Functional")
+        {
+        }
+    }
+}
